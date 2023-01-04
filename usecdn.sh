e844cb4eeae3644ca823501052cfb88c2365a7e2
Mohd Izzudin <izzudinxamira@gmail.com>
2023-01-04 11:51:58 +0800

    UPDATE 4

--- usecdn.sh
+++ usecdn.sh
@@ -14,10 +14,22 @@ echo "ENDING 1ST CONVERTION"
 
 sleep 1
 
-echo "CONVERT PUBLIC TO CDN URL"
-# FILENAME2="dev.index.html"
-# NEWFILENAME2="dev.index.3.html"
-SEARCH2="public" 
-REPLACE2="https://d29539q498fyfe.cloudfront.net/public"
+echo "CONVERT public img"
+SEARCH2="public/img" 
+REPLACE2="https://d29539q498fyfe.cloudfront.net/public/img"
 sed -i '' "s,$SEARCH2,$REPLACE2,g" $FILENAMECDN
-echo "ENDING 2ND CONVERTION"
+
+echo "CONVERT public scriptjs"
+SEARCH3="public/script.js" 
+REPLACE3="https://d29539q498fyfe.cloudfront.net/public/script.js"
+sed -i '' "s,$SEARCH3,$REPLACE3,g" $FILENAMECDN
+
+echo "CONVERT public icons"
+SEARCH4="public/icons" 
+REPLACE4="https://d29539q498fyfe.cloudfront.net/public/icons"
+sed -i '' "s,$SEARCH4,$REPLACE4,g" $FILENAMECDN
+
+echo "CONVERT public css"
+SEARCH5="public/input" 
+REPLACE5="https://d29539q498fyfe.cloudfront.net/public/input"
+sed -i '' "s,$SEARCH5,$REPLACE5,g" $FILENAMECDN
